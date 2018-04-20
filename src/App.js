e8668729802a6070f4e019ba938f3558a4a4b40d
Evan Garrett <evang522@gmail.com>
2018-04-19 22:52:48 -0400

    Get General room layout generated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import ChatContainer from './Components/ChatContainer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* Header-Navbar */}
+
+        <main>
+          <div className='sidebar-container'>
+            <div className='room-title'>
+              Boat Room
+            </div>
+            <div className='channel-title'>
+              #Sales
+            </div>
+            <div className='channel-list-container'>
+              <h3> Channels:</h3>
+              <ul className='channel-list-ul'>
+                <li>Funzos</li>
+                <li>Meetups</li>
+                <li>Sales</li>
+              </ul>
+            </div>
+          </div>
+          <div className='header-chat-container'>
+            <div className='navbar-container'>
+            <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+          <a href="/">Hermes</a>
+        </Navbar.Brand>
+        </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">
+              Settings
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              
+            </NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.4}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>
+            </div>
+            <ChatContainer/>
+          </div>
+        </main>
       </div>
     );
   }
